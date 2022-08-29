8f13519188e21fdec934df5352e4eb5a9bc5a55b
TitusKenny <phuclachongphuc@gmail.com>
2022-08-29 17:51:48 +0700

    upload1

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Dacs.Startup))]
+namespace Dacs
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
